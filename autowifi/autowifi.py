56452d054a5168246bc638adc32e7cc890693d71
wudawei <univi@vip.qq.com>
2020-01-18 22:12:15 +0800

    Update autowifi.py

--- autowifi/autowifi.py
+++ autowifi/autowifi.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+# os用以使用系统命令，time打印时间或延时，requests发送http post。
 import os
 import time
 import requests
 
 
-# define hadears and data.
-
+# 准备http头部和数据，原自网页查看
 headers={
 "Host": "192.0.2.100",
 "User-Agent": "Mozilla/5.0",
@@ -35,28 +35,36 @@ data={
 }
 
 
-# cycle func.
-def cyc():
+# 定义循环子函数
+def autowifi():
     while (True):
+        # ping 外网3次，每次等5秒
         result = os.system(u"ping 1.2.4.8 -c 3 -W 5")
         if result != 0:
+            # 如果ping失败，主动重连，并重新获取ip
             try:
                 os.system(u"iw wlan0 disconnect")
+                # sleep让程序暂停，主要让前一个命令有充分的时间执行完
                 time.sleep(5)
                 os.system(u"iw wlan0 connect jabil_visitors")
                 time.sleep(5)
                 os.system(u"dhclient wlan0")
                 time.sleep(10)
+                # 模拟认证
                 r=requests.post('https://192.0.2.100/login.html',data=data,headers=headers,verify=False)
                 time.sleep(5)
+                # 打印当前时间和post的结果
                 print(time.asctime(time.localtime(time.time())),r.status_code)
             except Exception as e:
+                # 如果有报错，打印出来，但不结束程序
                 print(e)
         else:
+            # 如果ping成功，打印时间+normal
             print(time.asctime(time.localtime(time.time())),"normal")
         print("="*50)
-        time.sleep(1800)
+        # 程序休息10分钟
+        time.sleep(600)
 
-# call
+# 主语句调用函数
 if __name__=='__main__':
-    cyc()
+    autowifi()
